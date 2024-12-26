ddb2a02409377818fef2e1aa49279404881a3057
lyubolp <lyubo.karev@gmail.com>
2024-12-26 15:55:34 +0200

    Logger now outputs to both stdout and file. Added formatting

--- grader/utils/logger.py
+++ grader/utils/logger.py
@@ -6,4 +6,8 @@ from typing import Optional
 def setup_logger(student_id: Optional[str] = None):
     student_id = student_id or "grader"
     logger = logging.getLogger("grader")
-    logging.basicConfig(filename=student_id + ".log", level=logging.DEBUG)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler(), logging.FileHandler(student_id + ".log")],
+    )

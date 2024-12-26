d0a090bf71cf54919e6c41b53aed0c528f194337
lyubolp <lyubo.karev@gmail.com>
2024-12-26 15:56:34 +0200

    Checks now receive the project root as an argument

--- grader/checks/abstract_check.py
+++ grader/checks/abstract_check.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class AbstractCheck(ABC):
-    def __init__(self, name: str, max_points: int):
+    def __init__(self, name: str, max_points: int, project_root: str):
         self._name = name
         self._max_points = max_points
+        self._project_root = project_root
 
     @abstractmethod
     def run(self) -> float:

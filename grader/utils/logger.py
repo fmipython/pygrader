12adb0d41e0d2d31c56962ed733a3c1789ef3432
lyubolp <lyubo.karev@gmail.com>
2024-12-27 14:56:23 +0200

    Changes to the logger module

--- grader/utils/logger.py
+++ grader/utils/logger.py
@@ -3,11 +3,17 @@ import logging
 from typing import Optional
 
 
-def setup_logger(student_id: Optional[str] = None):
+def setup_logger(student_id: Optional[str] = None, debug_mode: bool = False) -> logging.Logger:
     student_id = student_id or "grader"
     logger = logging.getLogger("grader")
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.DEBUG if debug_mode else logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[logging.StreamHandler(), logging.FileHandler(student_id + ".log")],
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(student_id + ".log"),
+        ],
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
+
+    return logger

51704b4ca49ca76eae1af999f4c475e1eb473c49
lyubolp <lyubo.karev@gmail.com>
2024-12-27 14:56:51 +0200

    Added file-related utils

---
+++ grader/utils/files.py
@@ -0,0 +1,18 @@
+import os
+
+
+def find_all_python_files(project_root_dir: str) -> list[str]:
+    """
+    Find all python files in the project directory
+
+    :param project_root_dir: The path to the project directory
+    :return: A list of all python files in the project directory
+    """
+    python_files = [
+        os.path.join(root, file)
+        for root, _, files in os.walk(project_root_dir)
+        for file in files
+        if file.endswith(".py") and ".venv" not in root
+    ]
+
+    return python_files

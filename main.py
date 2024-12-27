317769066e50f059b05ae0380668641b9ac5c57c
lyubolp <lyubo.karev@gmail.com>
2024-12-27 22:11:32 +0200

    Debug is now verbose, also checks are printed at the end

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from grader.checks.pylint_check import PylintCheck
 
 if __name__ == "__main__":
     args = get_args()
-    logger = setup_logger(args["student_id"], debug_mode=args["debug"])
+    logger = setup_logger(args["student_id"], verbosity=args["verbosity"])
 
-    logger.info("Python project grader, v0.1")
+    logger.verbose("Python project grader, v0.1")
     logger.debug("Arguments: %s", args)
 
     scores = []
     with VirtualEnvironment(args["project_root"]) as venv:
         pylint = PylintCheck("pylint", 3, args["project_root"])
         scores.append((pylint.name, pylint.run()))
+
+    for check, score in scores:
+        logger.info("Check: %s, Score: %s", check, score)

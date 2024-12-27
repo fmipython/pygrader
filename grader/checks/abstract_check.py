b0c756b730ac22a030196248c8c57f976469b252
lyubolp <lyubo.karev@gmail.com>
2024-12-27 15:10:48 +0200

    Added the ability to pass a custom function to calculate the score

--- grader/checks/abstract_check.py
+++ grader/checks/abstract_check.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import Callable
 
 
 class AbstractCheck(ABC):
-    def __init__(self, name: str, max_points: int, project_root: str):
+    def __init__(self, name: str, max_points: int, project_root: str, scores_mapping: Callable[[float], float]):
         self._name = name
         self._max_points = max_points
         self._project_root = project_root
+        self._scores_mapping = scores_mapping
 
     @abstractmethod
     def run(self) -> float:

d9f4335d1981907c06449da8ba2549ca90bd94c9
lyubolp <lyubo.karev@gmail.com>
2024-12-27 21:47:19 +0200

    Moved some things into debug mode

--- grader/utils/logger.py
+++ grader/utils/logger.py
@@ -6,9 +6,17 @@ from typing import Optional
 def setup_logger(student_id: Optional[str] = None, debug_mode: bool = False) -> logging.Logger:
     student_id = student_id or "grader"
     logger = logging.getLogger("grader")
+
+    level = logging.DEBUG if debug_mode else logging.INFO
+
+    if debug_mode:
+        log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    else:
+        log_format = "%(levelname)s - %(message)s"
+
     logging.basicConfig(
-        level=logging.DEBUG if debug_mode else logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=level,
+        format=log_format,
         handlers=[
             logging.StreamHandler(),
             logging.FileHandler(student_id + ".log"),

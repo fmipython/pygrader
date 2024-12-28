16a2918b06c54627ceffae700add0b2567868821
lyubolp <lyubo.karev@gmail.com>
2024-12-28 23:06:11 +0200

    Added constants file

---
+++ grader/utils/constants.py
@@ -0,0 +1,12 @@
+"""
+Module containing the constants
+"""
+
+import os
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__name__))
+
+REPORTS_TEMP_DIR = os.path.join(ROOT_DIR, "reports")
+
+MYPY_TYPE_HINT_CONFIG = os.path.join(ROOT_DIR, "config", "mypy_type_hints_2024.ini")
+MYPY_LINE_COUNT_REPORT = os.path.join(REPORTS_TEMP_DIR, "linecount.txt")

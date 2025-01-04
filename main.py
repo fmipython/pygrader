cf4f62303fe0ee93202e5612e16ff9176d8d4cab
lyubolp <lyubo.karev@gmail.com>
2025-01-04 13:08:47 +0200

    Moved things around in main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import logging
 from grader.utils.cli import get_args
-from grader.utils.logger import setup_logger
-from grader.utils.virtual_environment import VirtualEnvironment
+
+# from grader.utils.virtual_environment import VirtualEnvironment
 from grader.checks.pylint_check import PylintCheck
+from grader.checks.type_hints_check import TypeHintsCheck
+from grader.utils.logger import setup_logger
 
 if __name__ == "__main__":
     args = get_args()
@@ -11,9 +14,15 @@ if __name__ == "__main__":
     logger.debug("Arguments: %s", args)
 
     scores = []
-    with VirtualEnvironment(args["project_root"]) as venv:
-        pylint = PylintCheck("pylint", 3, args["project_root"])
-        scores.append((pylint.name, pylint.run()))
+
+    pylint = PylintCheck("pylint", 3, args["project_root"])
+    scores.append((pylint.name, pylint.run()))
+
+    type_hints = TypeHintsCheck("type_hints", 2, args["project_root"])
+    scores.append((type_hints.name, type_hints.run()))
+
+    # with VirtualEnvironment(args["project_root"]) as venv:
+    #     pass
 
     for check, score in scores:
         logger.info("Check: %s, Score: %s", check, score)

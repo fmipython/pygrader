bd24349bb5c7af4b761d9ee6c1957cab67dbf796
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-05 15:54:30 +0200

    Added docstrings to pylint_check and removed the scores mapping argument

--- grader/checks/abstract_check.py
+++ grader/checks/abstract_check.py
@@ -1,26 +1,42 @@
+"""
+Module containing a class representing an abstract check.
+Each check should inherit from this class.
+"""
 from abc import ABC, abstractmethod
-from typing import Callable, Optional
 
 
 class AbstractCheck(ABC):
-    def __init__(
-        self, name: str, max_points: int, project_root: str, scores_mapping: Optional[Callable[[float], float]] = None
-    ):
+    """
+    Each check has a name and a maximum amount of points.
+    It also needs the project root path.
+    """
+    def __init__(self, name: str, max_points: int, project_root: str):
         self._name = name
         self._max_points = max_points
         self._project_root = project_root
-        self._scores_mapping = scores_mapping
 
         # TODO - Add a check if running inside a virtual environment
 
     @abstractmethod
     def run(self) -> float:
-        pass
+        """
+        Main method that executes the check.
+
+        Returns the score of the check.
+        """
 
     @property
     def name(self) -> str:
+        """
+        Get the name of the check.
+
+        Returns the name of the check
+        """
         return self._name
 
     @property
     def max_points(self) -> int:
+        """
+        Returns the maximum amount of points that can be achieved by the check.
+        """
         return self._max_points

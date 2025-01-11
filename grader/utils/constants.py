e0f72e2f8ed8c411e3cc10c73cde86f1b4382a90
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-11 13:06:18 +0200

    Added new constants

--- grader/utils/constants.py
+++ grader/utils/constants.py
@@ -11,5 +11,15 @@ REPORTS_TEMP_DIR = os.path.join(ROOT_DIR, "reports")
 MYPY_TYPE_HINT_CONFIG = os.path.join(ROOT_DIR, "config", "mypy_type_hints_2024.ini")
 MYPY_LINE_COUNT_REPORT = os.path.join(REPORTS_TEMP_DIR, "linecount.txt")
 
-
 REQUIREMENTS_FILENAME = "requirements.txt"
+
+VENV_NAME = ".venv"
+
+COVERAGE_BIN = "coverage"
+COVERAGE_PATH = os.path.join(VENV_NAME, "bin", COVERAGE_BIN)
+
+
+COVERAGE_RUN_ARGS = ["run", "-m"]
+COVERAGE_RUN_PYTEST_ARGS = ["pytest"]
+
+COVERAGE_REPORT_ARGS = ["report", "--omit='tests*/*", "--format=total"]

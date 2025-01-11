a8f6a8953c2bc7a31136f1e2036bb3674c284ffc
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-11 13:06:41 +0200

    Added coverage check to main

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 Main entry point of the program.
 Calls all the checks, and stores their results
 """
-from grader.utils.cli import get_args
 
-# from grader.utils.virtual_environment import VirtualEnvironment
+from grader.checks.coverage_check import CoverageCheck
 from grader.checks.pylint_check import PylintCheck
 from grader.checks.type_hints_check import TypeHintsCheck
 from grader.checks.requirements_check import RequirementsCheck
+from grader.utils.cli import get_args
 from grader.utils.logger import setup_logger, VERBOSE
+from grader.utils.virtual_environment import VirtualEnvironment
 
 if __name__ == "__main__":
     args = get_args()
@@ -28,8 +29,9 @@ if __name__ == "__main__":
     type_hints = TypeHintsCheck("type_hints", 2, args["project_root"])
     scores.append((type_hints.name, type_hints.run()))
 
-    # with VirtualEnvironment(args["project_root"]) as venv:
-    #     pass
+    with VirtualEnvironment(args["project_root"]) as venv:
+        coverage = CoverageCheck("coverage", 5, args["project_root"])
+        scores.append((coverage.name, coverage.run()))
 
     for check, score in scores:
         logger.info("Check: %s, Score: %s", check, score)

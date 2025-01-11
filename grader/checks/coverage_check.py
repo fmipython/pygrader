7e4d3a1cf6b346018974510fd7948288492038ce
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-11 13:06:30 +0200

    Implemented the coverage check

--- grader/checks/coverage_check.py
+++ grader/checks/coverage_check.py
@@ -2,8 +2,11 @@
 Module containing the unit test code coverage check.
 """
 import logging
+import os
+import subprocess
 
 from grader.checks.abstract_check import AbstractCheck
+from grader.utils.constants import COVERAGE_PATH, COVERAGE_RUN_ARGS, COVERAGE_RUN_PYTEST_ARGS, COVERAGE_REPORT_ARGS
 from grader.utils.logger import VERBOSE
 
 logger = logging.getLogger("grader")
@@ -16,6 +19,8 @@ class CoverageCheck(AbstractCheck):
     def __init__(self, name: str, max_points: int, project_root: str):
         super().__init__(name, max_points, project_root)
 
+        self.__coverage_full_path = os.path.join(project_root, COVERAGE_PATH)
+
     def run(self) -> float:
         """
         Run the coverage check on the project.
@@ -24,6 +29,47 @@ class CoverageCheck(AbstractCheck):
         """
         logger.log(VERBOSE, "Running %s", self.name)
 
-        # TODO - Implement the coverage check
+        is_coverage_run_okay = self.__coverage_run()
 
-        return 0.0
+        if not is_coverage_run_okay:
+            return 0.0
+
+        coverage_report_result = self.__coverage_report()
+
+        if coverage_report_result is None:
+            return 0.0
+
+        return self.__translate_score(coverage_report_result)
+
+    def __translate_score(self, coverage_score: float) -> float:
+        """
+        The coverage score is a percentage of the amount of lines covered in the project.
+        The number is between 0 and max_points.
+        """
+        return coverage_score
+
+    def __coverage_run(self):
+        """
+        Run the coverage tool on the project.
+        """
+        command = [self.__coverage_full_path] + COVERAGE_RUN_ARGS + COVERAGE_RUN_PYTEST_ARGS
+        output = subprocess.run(command, check=False, capture_output=True)
+
+        if output.returncode != 0:
+            logger.error("Coverage run failed: %s", output.stderr)
+            return False
+
+        return True
+
+    def __coverage_report(self):
+        """
+        Generate a report from the coverage tool.
+        """
+        command = [self.__coverage_full_path] + COVERAGE_REPORT_ARGS
+        output = subprocess.run(command, check=False, capture_output=True)
+
+        if output.returncode != 0:
+            logger.error("Coverage report failed: %s", output.stderr)
+            return None
+
+        return int(output.stdout)

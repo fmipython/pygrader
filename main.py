5027f0a6344b2e60c576e10e281c26bbc4b642b8
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-12 12:43:32 +0200

    Output improvements

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ Main entry point of the program.
 Calls all the checks, and stores their results
 """
 
+import grader.utils.constants as const
+
 from grader.checks.coverage_check import CoverageCheck
 from grader.checks.pylint_check import PylintCheck
 from grader.checks.type_hints_check import TypeHintsCheck
 from grader.checks.requirements_check import RequirementsCheck
 from grader.utils.cli import get_args
 from grader.utils.files import get_tests_directory_name
-from grader.utils.logger import setup_logger, VERBOSE
+from grader.utils.logger import setup_logger
 from grader.utils.virtual_environment import VirtualEnvironment
 
 if __name__ == "__main__":
     args = get_args()
     logger = setup_logger(args["student_id"], verbosity=args["verbosity"])
 
-    logger.log(VERBOSE, "Python project grader, v0.1")
+    logger.info("Python project grader, %s", const.VERSION)
+    logger.info("Running checks for student %s", args["student_id"])
     logger.debug("Arguments: %s", args)
 
     tests_directory = get_tests_directory_name(args["project_root"])
@@ -26,17 +29,17 @@ if __name__ == "__main__":
     scores = []
 
     requirements = RequirementsCheck("requirements.txt", 1, args["project_root"])
-    scores.append((requirements.name, requirements.run()))
+    scores.append((requirements.name, requirements.run(), requirements.max_points))
 
     pylint = PylintCheck("pylint", 3, args["project_root"])
-    scores.append((pylint.name, pylint.run()))
+    scores.append((pylint.name, pylint.run(), pylint.max_points))
 
     type_hints = TypeHintsCheck("type_hints", 2, args["project_root"])
-    scores.append((type_hints.name, type_hints.run()))
+    scores.append((type_hints.name, type_hints.run(), type_hints.max_points))
 
     with VirtualEnvironment(args["project_root"]) as venv:
         coverage = CoverageCheck("coverage", 5, args["project_root"])
-        scores.append((coverage.name, coverage.run()))
+        scores.append((coverage.name, coverage.run(), coverage.max_points))
 
-    for check, score in scores:
-        logger.info("Check: %s, Score: %s", check, score)
+    for check, score, max_score in scores:
+        logger.info("Check: %s, Score: %s/%s", check, score, max_score)

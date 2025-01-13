05a3c12407598231a901b2590edb598b485e533f
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-13 19:09:43 +0200

    Added a module for working with the config

---
+++ grader/utils/config.py
@@ -0,0 +1,14 @@
+"""
+Module for loading the configuration file.
+"""
+import json
+
+
+def load_config(config_file_path: str) -> dict:
+    """
+    Load the configuration file.
+    :param config_file_path: Path to the configuration file.
+    :return: The configuration as a dictionary.
+    """
+    with open(config_file_path, encoding="utf-8") as config_file:
+        return json.load(config_file)

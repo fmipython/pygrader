7144598592d97af6b5366a732188acd81d101efd
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-01-13 19:09:20 +0200

    Added a factory method for the checks

---
+++ grader/checks/checks_factory.py
@@ -0,0 +1,43 @@
+"""
+Factory for creating the checks objects.
+"""
+
+from grader.checks.abstract_check import AbstractCheck
+
+from grader.checks.coverage_check import CoverageCheck
+from grader.checks.pylint_check import PylintCheck
+from grader.checks.requirements_check import RequirementsCheck
+from grader.checks.type_hints_check import TypeHintsCheck
+
+
+NAME_TO_CHECK = {
+    "coverage": CoverageCheck,
+    "pylint": PylintCheck,
+    "requirements": RequirementsCheck,
+    "type-hints": TypeHintsCheck,
+}
+
+
+def create_checks(config: dict, project_root: str) -> tuple[list[AbstractCheck], list[AbstractCheck]]:
+    checks = config["checks"]
+
+    non_venv_checks = []
+    venv_checks = []
+    for check in checks:
+        name = check["name"]
+        max_points = check["max_points"]
+
+        if name not in NAME_TO_CHECK:
+            raise ValueError(f"Unknown check name: {name}")  # TODO - Replace with custom exception
+
+        is_venv = check.get("venv", False)
+
+        check_class = NAME_TO_CHECK[name]
+        created_check = check_class(name, max_points, project_root)
+
+        if is_venv:
+            venv_checks.append(created_check)
+        else:
+            non_venv_checks.append(created_check)
+
+    return non_venv_checks, venv_checks
--- main.py
+++ main.py
@@ -5,41 +5,47 @@ Calls all the checks, and stores their results
 
 import grader.utils.constants as const
 
-from grader.checks.coverage_check import CoverageCheck
-from grader.checks.pylint_check import PylintCheck
-from grader.checks.type_hints_check import TypeHintsCheck
-from grader.checks.requirements_check import RequirementsCheck
+from grader.checks.checks_factory import create_checks
 from grader.utils.cli import get_args
+from grader.utils.config import load_config
 from grader.utils.files import get_tests_directory_name
 from grader.utils.logger import setup_logger
 from grader.utils.virtual_environment import VirtualEnvironment
 
 if __name__ == "__main__":
     args = get_args()
-    logger = setup_logger(args["student_id"], verbosity=args["verbosity"])
+    student_id = args["student_id"]
+    logger = setup_logger(student_id, verbosity=args["verbosity"])
 
     logger.info("Python project grader, %s", const.VERSION)
-    logger.info("Running checks for student %s", args["student_id"])
+
+    try:
+        config = load_config(args["config_file"])
+    except FileNotFoundError as exc:
+        logger.error("Configuration file not found")
+        logger.debug("Exception: %s", exc)
+        exit(1)
+
+    logger.info("Running checks for student %s", student_id)
     logger.debug("Arguments: %s", args)
 
-    tests_directory = get_tests_directory_name(args["project_root"])
+    project_root = args["project_root"]
+
+    tests_directory = get_tests_directory_name(project_root)
     if tests_directory is None:
         logger.warning("No tests directory found in the project directory. Either it is missing or named differently.")
 
     scores = []
+    non_venv_checks, venv_checks = create_checks(config, project_root)
 
-    requirements = RequirementsCheck("requirements.txt", 1, args["project_root"])
-    scores.append((requirements.name, requirements.run(), requirements.max_points))
+    for check in non_venv_checks:
+        check_score = check.run()
+        scores.append((check.name, check_score, check.max_points))
 
-    pylint = PylintCheck("pylint", 3, args["project_root"])
-    scores.append((pylint.name, pylint.run(), pylint.max_points))
+    with VirtualEnvironment(project_root) as venv:
+        for check in venv_checks:
+            check_score = check.run()
+            scores.append((check.name, check_score, check.max_points))
 
-    type_hints = TypeHintsCheck("type_hints", 2, args["project_root"])
-    scores.append((type_hints.name, type_hints.run(), type_hints.max_points))
-
-    with VirtualEnvironment(args["project_root"]) as venv:
-        coverage = CoverageCheck("coverage", 5, args["project_root"])
-        scores.append((coverage.name, coverage.run(), coverage.max_points))
-
-    for check, score, max_score in scores:
-        logger.info("Check: %s, Score: %s/%s", check, score, max_score)
+    for name, score, max_score in scores:
+        logger.info("Check: %s, Score: %s/%s", name, score, max_score)

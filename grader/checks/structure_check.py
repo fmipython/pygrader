97407b0e7e1bf83fc2bf2cbdda11e597f16f70dd
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-03-20 08:16:59 +0200

    Refactor StructureCheck to utilize StructureValidator and streamline structure validation

--- grader/checks/structure_check.py
+++ grader/checks/structure_check.py
@@ -4,32 +4,17 @@ It checks if the project structure is correct.
 """
 
 import logging
-from dataclasses import dataclass
-from pathlib import Path
 
 import yaml
 
 from grader.checks.abstract_check import NonScoredCheck, CheckError
 from grader.utils.logger import VERBOSE
+from grader.utils.structure_validator import StructureValidator
 
 logger = logging.getLogger("grader")
 
 
-# TODO - This whole thing can be re-used to extract tests, source code, etc.
-# Still need to think about how though.
-
 # TODO - It also needs to work with multiple structures, depending on the project type.
-
-
-@dataclass
-class StructureInformation:
-    """
-    Dataclass representing the structure information.
-    """
-
-    name: str
-    required: bool
-    patterns: list[str]
 
 
 class StructureCheck(NonScoredCheck):
@@ -54,7 +39,7 @@ class StructureCheck(NonScoredCheck):
         structure_elements = StructureCheck.__load_structure_file(self.__structure_file)
 
         for element in structure_elements:
-            is_element_valid = self.__is_structure_valid(element)
+            is_element_valid = element.is_structure_valid(self._project_root)
 
             logger.log(VERBOSE, "Is %s structure valid ? %s", element.name, is_element_valid)
 
@@ -64,7 +49,7 @@ class StructureCheck(NonScoredCheck):
         return True
 
     @staticmethod
-    def __load_structure_file(filepath: str) -> list[StructureInformation]:
+    def __load_structure_file(filepath: str) -> list[StructureValidator]:
         """
         Read the structure YAML file and return the structure information.
 
@@ -78,38 +63,7 @@ class StructureCheck(NonScoredCheck):
             raw_structure = yaml.safe_load(file_pointer)
 
         try:
-            elements = [build_structure_information(value) for value in raw_structure.values()]
+            elements = [StructureValidator.from_dict(value) for value in raw_structure.values()]
         except KeyError as error:
             raise CheckError(f"Invalid structure file: {error}") from error
         return elements
-
-    def __is_structure_valid(self, structure_element: StructureInformation) -> bool:
-        """
-        A structure is valid if all patterns match at least one file in the project.
-
-        :param structure_element: The structure element to check
-        :type structure_element: StructureInformation
-        :return: Whether the structure is valid
-        :rtype: bool
-        """
-        path = Path(self._project_root)
-        for pattern in structure_element.patterns:
-            if not any(path.glob(pattern)):
-                return False
-        return True
-
-
-def build_structure_information(raw_object: dict) -> StructureInformation:
-    """
-    Parse the YAML contents into a StructureInformation object.
-
-    :param raw_object: The read YAML object
-    :type raw_object: dict
-    :return: The parsed structure information
-    :rtype: StructureInformation
-    """
-    name = raw_object["name"]
-    required = raw_object["required"]
-    patterns = raw_object["patterns"]
-
-    return StructureInformation(name, required, patterns)

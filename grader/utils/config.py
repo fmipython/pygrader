49fbd28353886a1411f79e301154e2a67d147a32
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-09-09 19:22:22 +0300

    Add bad weather handling

--- grader/utils/config.py
+++ grader/utils/config.py
@@ -3,16 +3,29 @@ Module for loading the configuration file.
 """
 
 import json
+from grader.utils.external_resources import is_resource_remote, download_file_from_url, ExternalResourceError
 
 
-def load_config(config_file_path: str) -> dict:
+def load_config(config_path: str) -> dict:
     """
     Load the configuration file.
-    :param config_file_path: Path to the configuration file.
+    :param config_path: Path or URL to the configuration file.
     :return: The configuration as a dictionary.
     """
-    with open(config_file_path, encoding="utf-8") as config_file:
-        return json.load(config_file)
+
+    if is_resource_remote(config_path):
+        try:
+            config_path = download_file_from_url(config_path)
+        except ExternalResourceError as e:
+            raise InvalidConfigError(f"Could not load configuration from {config_path}") from e
+
+    try:
+        with open(config_path, encoding="utf-8") as config_file:
+            return json.load(config_file)
+    except FileNotFoundError as e:
+        raise InvalidConfigError(f"Configuration file not found: {config_path}") from e
+    except json.JSONDecodeError as e:
+        raise InvalidConfigError(f"Error parsing JSON configuration file: {config_path}") from e
 
 
 class InvalidConfigError(Exception):

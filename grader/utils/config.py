2c41c72549f398072fd2e78bc719a2505dc02735
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-09-13 12:28:36 +0300

    Improve exception variable naming

--- grader/utils/config.py
+++ grader/utils/config.py
@@ -16,16 +16,16 @@ def load_config(config_path: str) -> dict:
     if is_resource_remote(config_path):
         try:
             config_path = download_file_from_url(config_path)
-        except ExternalResourceError as e:
-            raise InvalidConfigError(f"Could not load configuration from {config_path}") from e
+        except ExternalResourceError as exc:
+            raise InvalidConfigError(f"Could not load configuration from {config_path}") from exc
 
     try:
         with open(config_path, encoding="utf-8") as config_file:
             return json.load(config_file)
-    except FileNotFoundError as e:
-        raise InvalidConfigError(f"Configuration file not found: {config_path}") from e
-    except json.JSONDecodeError as e:
-        raise InvalidConfigError(f"Error parsing JSON configuration file: {config_path}") from e
+    except FileNotFoundError as exc:
+        raise InvalidConfigError(f"Configuration file not found: {config_path}") from exc
+    except json.JSONDecodeError as exc:
+        raise InvalidConfigError(f"Error parsing JSON configuration file: {config_path}") from exc
 
 
 class InvalidConfigError(Exception):

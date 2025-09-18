36b32a0e7d334a8466d2bf09f24fdca95ff795f9
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-09-18 19:15:03 +0300

    Refactor placeholder handling in JSON loading function

--- grader/utils/json_with_templates.py
+++ grader/utils/json_with_templates.py
@@ -1,4 +1,8 @@
 import json
+import re
+
+
+pattern = re.compile(r"\$\{\{(\w+)\}\}")
 
 
 def load_with_values(file_path: str, **kwargs: str) -> dict:
@@ -11,7 +15,15 @@ def load_with_values(file_path: str, **kwargs: str) -> dict:
     with open(file_path, encoding="utf-8") as config_file:
         content = config_file.read()
 
+    matches = set(pattern.findall(content))
+    for key in matches:
+        if key not in kwargs:
+            raise ValueError(f"Missing value for placeholder: {key}")
+
     for key, value in kwargs.items():
+        if key not in matches:
+            raise ValueError(f"Unexpected value for placeholder: {key}")
+
         placeholder = "${{" + key + "}}"
         content = content.replace(placeholder, str(value))
 

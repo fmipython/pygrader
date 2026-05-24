3f32d0a59e60fd36f6e186b86135ca2fa57e835b
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-05-24 14:46:25 +0300

    Cove config is now a dataclass

--- grader/utils/cove_config.py
+++ grader/utils/cove_config.py
@@ -1,17 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class CoveConfig:
-    def __init__(self, base_url: str, api_key: str, project_name: str):
-        self.__base_url = base_url
-        self.__api_key = api_key
-        self.__project_name = project_name
-
-    @property
-    def base_url(self) -> str:
-        return self.__base_url
-
-    @property
-    def api_key(self) -> str:
-        return self.__api_key
-
-    @property
-    def project_name(self) -> str:
-        return self.__project_name
+    base_url: str
+    api_key: str
+    project_id: str
